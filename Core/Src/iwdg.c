f0ddb9d257da2063a28134599569541df8a003ea
samkwok <mskwok@hkclr.hk>
2024-01-09 11:43:15 +0800

    - Update IWDG configuration

--- Core/Src/iwdg.c
+++ Core/Src/iwdg.c
@@ -38,8 +38,8 @@ void MX_IWDG_Init(void)
 
   /* USER CODE END IWDG_Init 1 */
   hiwdg.Instance = IWDG;
-  hiwdg.Init.Prescaler = IWDG_PRESCALER_256;
-  hiwdg.Init.Reload = 125-1;
+  hiwdg.Init.Prescaler = IWDG_PRESCALER_16;
+  hiwdg.Init.Reload = 2500-1;
   if (HAL_IWDG_Init(&hiwdg) != HAL_OK)
   {
     Error_Handler();
